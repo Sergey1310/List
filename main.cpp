83247ca75d59369b3613af4e211cc09af164c3e4
Sergey <Sergey@pingvin.club>
2021-10-01 19:00:12 +0300

    New version List.

--- main.cpp
+++ main.cpp
@@ -21,233 +21,179 @@ public:
     void Revers();
     int& operator[] (const int index);
 
-private:
-    class Node
-    {
-    public:
-        Node *pNext;
-        Node *pLast;
-        int data;
+    void PrintSize(){
+        std::cout << "Size = " << size << std::endl;
+    }
 
-        Node(int data = 0, Node *pNext = nullptr, Node *pLast = nullptr)
+private:
+    struct Node
+    {
+        int data;
+        Node *pNext;
+        Node *pPrev;
+
+        Node(int data = 0, Node *pNext = nullptr, Node *pPrev = nullptr)
         {
             this->data = data;
             this->pNext = pNext;
-            this->pLast = pLast;
+            this->pPrev = pPrev;
         }
+
     };
+
+
 
     int size;
     Node *head;
+    Node *tail;
+
+    friend std::ostream &operator<<(std::ostream &os, List a);
+
+
 };
 
-
-
-List::List() {
-    size = 0;
-    head = nullptr;
-}
-List::~List() {
-    Clear();
-}
-
-void List::PushBack(int data) {
-    if (head == nullptr)
+std::ostream &operator<<(std::ostream &os, List a) {
+    int count = 0;
+    List::Node *current = a.head;
+    while (current)
     {
-        head = new Node (data);
+        ++count;
+        std::cout << count << "\t Data = " << current->data << "\t Current address = " << current;
+        std::cout << "\t Previous address = " << current->pPrev << "\t Next address = " << current->pNext << std::endl;
+        current = current->pNext;
     }
+};
+
+List::List():head(nullptr), tail(nullptr), size(0) {}
+
+void List::PushBack(int data)
+{
+    if (!head)
+        head = tail = new Node (data);
     else
     {
-        Node *current = this->head;
-        while (current->pNext != nullptr)
-        {
-            current = current->pNext;
-        }
-        current->pNext = new Node(data);
-        Node *last = current;
-        current = current->pNext;
-        current->pLast = last;
+        Node *temp = new Node (data, nullptr, tail);
+        tail->pNext = temp;
+        tail = temp;
     }
-    size++;
+    ++size;
+
 }
-void List::PushFront(int data) {
-    head = new Node(data, head);
-    Node *temp = head->pNext;
-    temp->pLast = head;
+void List::PushFront(int data)
+{
+    if (!head)
+        head = tail = new Node (data);
+    else
+    {
+        Node *temp = new Node(data,head);
+        head->pPrev = temp;
+        head = temp;
+    }
     ++size;
 }
 void List::Insert(int data, int index) {
-
-    if (!index)
-    {
-        PushFront(data);
-    }else if (index==size-1)
-    {
+    if (!head)
         PushBack(data);
-    } else
+    else if (index == 1)
+        PushFront(data);
+    else if (index == size)
+        PushBack(data);
+    else
     {
-        Node *prev = this->head;
-        for (int i = 0; i < index-1; ++i) {
-            prev = prev->pNext;
+        Node *PREV;
+        Node *NEXT;
+        Node *temp;
+        if ((size - index) >= (size/2))
+        {
+            PREV = NEXT = head;
+            for (int i = 1; i < index; ++i) {
+                PREV = PREV->pNext;
+                NEXT = NEXT->pNext;
+            }
+            PREV = PREV->pPrev;
+            temp = new Node(data,NEXT,PREV);
+            PREV->pNext = temp;
+            NEXT->pPrev = temp;
+            ++size;
         }
-        Node *newNode = new Node(data,prev->pNext,prev);
-        Node *temp = newNode;
-        prev->pNext = newNode;
-        for (int i = 0; i < 2; ++i) {
-            prev = prev->pNext;
+        else
+        {
+            PREV = NEXT = tail;
+            int i = size - index;
+            for (int j = 0; j < i; ++j)
+            {
+                PREV = PREV->pPrev;
+                NEXT = NEXT->pPrev;
+            }
+            PREV = PREV->pPrev;
+            temp = new Node(data,NEXT,PREV);
+            PREV->pNext = temp;
+            NEXT->pPrev = temp;
+            ++size;
         }
-        prev->pLast=temp;
-        ++size;
     }
 
 }
 
-void List::Clear() {
-    while (size-1)
+List::~List() {}
+
+void List::Clear()
+{
+    while (head!=tail)
     {
         PopFront();
     }
-}
-void List::PopBack() {
-    Node *temp = head;
-    Node *toDel;
-    while (temp->pNext != nullptr)
-    {
-        temp = temp->pNext;
-    }
-    temp = temp->pLast;
-    toDel = temp->pNext;
-    delete toDel;
-    temp->pNext = nullptr;
+    delete head;
+    head = tail =nullptr;
     --size;
 }
-void List::PopFront() {
-    Node *temp = head;
-    head = head->pNext;
-    head->pLast= nullptr;
+
+void List::PopBack()
+{
+    Node *temp;
+    tail = tail->pPrev;
+    temp = tail->pNext;
+    tail->pNext = nullptr;
     delete temp;
     --size;
 }
-void List::Remove(int index) {
-    if (!index){PopFront();}
-    else if (index==size-1){PopBack();}
-    else
-    {
-        Node *post = this->head;
-        Node *prev = this->head;
-        for (int i = 0; i < index-1; ++i) {
-            prev = prev->pNext;
-        }
-        for (int i = 0; i < index+1; ++i) {
-            post = post->pNext;
-        }
-        Node *toDel = prev->pNext;
-        delete toDel;
-        prev->pNext = post;
-        post->pLast = prev;
-        --size;
 
-    }
-}
-
-int List::GetSize()
+void List::PopFront()
 {
-    return size;
-}
-void List::Print() {
-    Node *current = this->head;
-    while (current->pNext != nullptr)
-    {
-        std::cout << "Data = " << current->data <<"\t This Link = " << current <<"\t Last link = " << current->pLast << "\t Next link = " << current->pNext << std::endl;
-        current = current->pNext;
-        if (current->pNext == nullptr) std::cout << "Data = " << current->data <<"\t This Link = " << current <<"\t Last link = " << current->pLast << "\t Next link = " << current->pNext << std::endl;
-
-    }
-}
-int &List::operator[](const int index) {
-    int counter = 0;
-    Node *current = this->head;
-    while (current != nullptr)
-    {
-        if (counter == index)
-        {
-            return current->data;
-        }
-        current = current->pNext;
-        ++counter;
-    }
-}
-int List::Sum() {
-    int sum = 0;
     Node *temp = head;
-    for (int i = 0; i < size; ++i)
-    {
-        sum += temp->data;
-        temp = temp->pNext;
-    }
-    return sum;
+    head = head->pNext;
+    head->pPrev= nullptr;
+    delete temp;
+    --size;
 }
 
-void List::Revers() {
-    Node *first = head;
-    Node *last = head;
-    while (last->pNext != nullptr)
-    {
-        last = last->pNext;
-    }
-    int temp;
-    for (int i = 0; i < (size/2); ++i) {
-        temp = first->data;
-        first->data = last->data;
-        last->data = temp;
-        first = first->pNext;
-        last = last->pLast;
-    }
-}
-
-
-void Show( List& lst )
-{
-    lst.Print();
-    std::cout << "Objects in list : " <<lst.GetSize() << std::endl << std::endl;
-}
 
 int main()
 {
     List lst;
-    lst.PushBack(15);
-    lst.PushBack(10);
+    lst.PushBack(1);
+    lst.PushBack(2);
+    lst.PushBack(3);
+    lst.PushBack(4);
     lst.PushBack(5);
-    lst.PushBack(100);
-
-    std::cout << "\n Sum = " << lst.Sum() << std::endl;
-
-    std::cout << "The second element of list = " << lst[1] << std::endl;
-    Show(lst);
-
-    lst.PopFront();
-    Show(lst);
-
-    lst.PushFront(99);
-    Show(lst);
-
-    lst.Insert(777,2);
-    Show(lst);
-
-    lst.Remove(2);
-    Show(lst);
-
-    lst.PushBack(666);
-    Show(lst);
+    lst.PushFront(0);
+    std::cout << lst;
+    lst.PrintSize();
+    lst.Insert(777, 6);
+    std::cout << lst;
+    lst.PrintSize();
 
     lst.PopBack();
-    Show(lst);
+    std::cout << lst;
+    lst.PrintSize();
 
-    lst.Revers();
-    Show(lst);
+    lst.PopFront();
+    std::cout << lst;
+    lst.PrintSize();
 
     lst.Clear();
-    Show(lst);
+    std::cout << lst;
+    lst.PrintSize();
 
     return 0;
 }
